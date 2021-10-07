77f23d6d2fb03b0c5e0014b0176486e0c4cbad35
IshitaKanwar04 <kanwar.ishita04@gmail.com>
2021-10-07 14:54:03 +0530

    Added custom input

--- Minor_1/Image Processing/src/decryptBit.java
+++ Minor_1/Image Processing/src/decryptBit.java
@@ -1,6 +1,5 @@
 
 
-
 import java.io.IOException;
 
 import java.awt.image.BufferedImage;
@@ -15,83 +14,97 @@ import java.util.Scanner;
 
 public class decryptBit {
 	
+	
 	public static void main(String args[]) throws IOException {
-		
-		
+	
 
-	       
-			
-			int num=0;
-			int Alpha=255;
-			BufferedImage encryptedImage = null,  decryptedImage=null; // for importing image 
-			File eImage, dImage;
-			
+       Scanner scan= new Scanner(System.in);
 		
-	        
-			
-			try //Images import
-			{
-			
-			 
-	                 
-	         eImage = new File("H:/hp/eclipse/programs/Image Processing/src/Output Images/Image is hidden here.png"); 
-			 encryptedImage = ImageIO.read(eImage);
+		int num=0;
+		
+		BufferedImage encryptedImage = null,  decryptedImage=null; // for importing image 
+		File eImage, dImage;
+		
+		System.out.println("Enter the path of the encrypted image: ");
+        String encrypted=scan.nextLine();
 
-			                  
-	       
-			
-			}
-			catch(Exception e)
-			{
-				System.out.println("----  ERROR!!!!! ----\n");
-				System.out.println("No Image Found\n");
-				e.printStackTrace();
-				System.out.println("\n\n-----  RESTART -----");
-				System.exit(0);
-			}
-			
-			int eHeight = encryptedImage.getHeight(); // getting height and width of each image
-			int eWidth  = encryptedImage.getWidth();
-			
-			
-			int[] eBlue = new int[eHeight*eWidth]; 
-			int[] eGreen = new int[eHeight*eWidth]; 
-			int[] eRed = new int[eHeight*eWidth]; 
-			num=0;
-			for(int y=0 ; y<eHeight ; y++)		// running the loop to cover all pixels of the image to be hidden in a rowwise manner
-			{
-				for(int x=0 ; x<eWidth ; x++)
-				{
-					int c = encryptedImage.getRGB(x,y);// Color class will return the colour of that pixel specified by x,y coordinates
-					eBlue[num] = (c & 0x000000F)<<4;// gets red component from c
-					eGreen[num] = (c & 0x00000F00)<<4;// gets green component from c 
-					eRed[num] = (c & 0x000F0000)<<4;// gets blue component from c
-										num++;
-				}	
-			}
-			
-			
-			
-			
-			
-			decryptedImage=createPixels(eRed,eGreen,eBlue,eHeight,eWidth);
-			
-			try
-			{
-				File outputfile = new File("H:/hp/eclipse/programs/Image Processing/src/Reterieved Images/reterieved.jpg");// where to store the decrypted image
-				ImageIO.write(decryptedImage, "png", outputfile);
-					
-			}
-			catch(IOException e)
-			{
-				System.out.println("----  ERROR!!!!! ----");
-				e.printStackTrace();
-			}
-			
-	System.out.println("Done");
+       
+        
+      
+        
+		
+		try //Images import
+		{
+		
+		 
+                 
+         eImage = new File(encrypted); 
+		 encryptedImage = ImageIO.read(eImage);
+
+		                  
+       
+		
 		}
-
-	public static BufferedImage createPixels(int[] Red, int[] Green, int[] Blue, int height, int width) {
+		
+		
+		catch(Exception e)
+		{
+			System.out.println("----  ERROR!!!!! ----\n");
+			System.out.println("No Image Found\n");
+			e.printStackTrace();
+			System.out.println("\n\n-----  RESTART -----");
+			System.exit(0);
+		}
+		
+		int eHeight = encryptedImage.getHeight(); // getting height and width of each image
+		int eWidth  = encryptedImage.getWidth();
+		
+		
+		int[] eBlue = new int[eHeight*eWidth]; 
+		int[] eGreen = new int[eHeight*eWidth]; 
+		int[] eRed = new int[eHeight*eWidth]; 
+		num=0;
+		for(int y=0 ; y<eHeight ; y++)		// running the loop to cover all pixels of the image to be hidden in a rowwise manner
+		{
+			for(int x=0 ; x<eWidth ; x++)
+			{
+				int c = encryptedImage.getRGB(x,y);// Color class will return the colour of that pixel specified by x,y coordinates
+				eBlue[num] = (c & 0x000000F)<<4;// gets red component from c
+				eGreen[num] = (c & 0x00000F00)<<4;// gets green component from c 
+				eRed[num] = (c & 0x000F0000)<<4;// gets blue component from c
+				
+				num++;
+			}	
+		}
+		
+		
+		
+		
+		
+		decryptedImage=createPixels(eRed,eGreen,eBlue,eHeight,eWidth);
+		System.out.println("Enetr the path of the image to be saved  : ");
+        String saveThis=scan.nextLine();
+		
+        try
+		{
+			File outputfile = new File(saveThis);// where to store the decrypted image
+			ImageIO.write(decryptedImage, "png", outputfile);
+				
+		}
+		
+        catch(IOException e)
+		{
+			System.out.println("----  ERROR!!!!! ----");
+			e.printStackTrace();
+		}
+		
+System.out.println("Done");
+}
+	
+	
+	
+	
+public static BufferedImage createPixels(int[] Red, int[] Green, int[] Blue, int height, int width) {
 		int pixel;
 		
 		BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
@@ -108,5 +121,5 @@ public class decryptBit {
 		
 		return(result);
 	}
-	
+
 }
